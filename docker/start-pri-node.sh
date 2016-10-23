93a14e245534a6133169a549a4690beefdccd6e8
suto <tong_su@hotmail.com>
2016-10-23 16:05:22 -0400

    remove hardcoded parent folder name for node

--- docker/start-pri-node.sh
+++ docker/start-pri-node.sh
@@ -3,10 +3,11 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 node_folder "
     exit 1
 fi
-
+project_root=/Users/suto/Documents/sandbox/ethereum/private-nodes
+project_root1=`pwd`
 #docker build -f Dockerfile -t mygeth:v2 .
 
 docker run -it --name my-geth-node \
 -p 30303:30303 -p 8545:8545 \
--v /Users/suto/Documents/sandbox/ethereum/private-nodes/$1:/ethereum/private-node \
+-v $project_root1/../$1:/ethereum/private-node \
 --rm mygeth:v2
