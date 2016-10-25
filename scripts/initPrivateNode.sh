f09842b291448f2abcf82219c6f7ad0c1ae7dd97
suto <tong_su@hotmail.com>
2016-10-24 21:52:02 -0400

    rename folder

---
+++ scripts/initPrivateNode.sh
@@ -0,0 +1,4 @@
+geth --datadir /ethereum/private-node/datadir init genesis.json
+
+geth --datadir /ethereum/private-node/datadir account new
+geth --datadir /ethereum/private-node/datadir account new
